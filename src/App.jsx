c166990b523c2ec6ea8e8de7039f97e75204aa82
RobFrontend <robert.grabowski97@gmail.com>
2024-02-06 16:24:53 +0100

    lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { Suspense, lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./Pages/HomePage";
-import AccountBalance from "./Pages/AccountBalance";
-import AccountSettings from "./Pages/AccountSettings";
-import Login from "./Pages/Login";
-import Register from "./Pages/Register";
-import AboutUs from "./Pages/AboutUs";
-import PageNotFound from "./Pages/PageNotFound";
+import Spinner from "./Components/Spinner";
+
+// import HomePage from "./Pages/HomePage";
+// import AccountBalance from "./Pages/AccountBalance";
+// import AccountSettings from "./Pages/AccountSettings";
+// import Login from "./Pages/Login";
+// import Register from "./Pages/Register";
+// import AboutUs from "./Pages/AboutUs";
+// import PageNotFound from "./Pages/PageNotFound";
+
+const HomePage = lazy(() => import("./Pages/HomePage"));
+const AccountBalance = lazy(() => import("./Pages/AccountBalance"));
+const AccountSettings = lazy(() => import("./Pages/AccountSettings"));
+const Login = lazy(() => import("./Pages/Login"));
+const Register = lazy(() => import("./Pages/Register"));
+const AboutUs = lazy(() => import("./Pages/AboutUs"));
+const PageNotFound = lazy(() => import("./Pages/PageNotFound"));
 
 // const BASE_URL = "http://localhost:9000";
 
@@ -13,15 +24,17 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-        <Routes>
-          <Route index element={<HomePage />} />
-          <Route path="balance" element={<AccountBalance />} />
-          <Route path="settings" element={<AccountSettings />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="aboutus" element={<AboutUs />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
+        <Suspense fallback={<Spinner />}>
+          <Routes>
+            <Route index element={<HomePage />} />
+            <Route path="balance" element={<AccountBalance />} />
+            <Route path="settings" element={<AccountSettings />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="aboutus" element={<AboutUs />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </div>
   );
---
+++ src/Components/Spinner.jsx
@@ -0,0 +1,11 @@
+import styles from "./Spinner.module.css";
+
+function Spinner() {
+  return (
+    <main className={styles.main}>
+      <h1>Loading...</h1>
+    </main>
+  );
+}
+
+export default Spinner;
